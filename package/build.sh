852cabfb6f53cb00a5e8d1be31c1df1b359680a8
Ben Armston <ben.armston@alces-software.com>
2018-08-22 12:00:38 +0100

    build.sh is more robust
    It is no longer dependent on being ran in the correct directory.

--- package/build.sh
+++ package/build.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
+set -euo pipefail
+
 package_name='flight-support-cli'
+build_dest='data/opt/flight-support'
 
 cw_ROOT="${cw_ROOT:-/opt/clusterware}"
 PATH="${cw_ROOT}"/opt/git/bin:"${cw_ROOT}"/opt/ruby/bin:$PATH
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+PACKAGE_DIR="${REPO_ROOT}/package"
+
+cd "${PACKAGE_DIR}"
 
 if [ -f ./${package_name}.zip ]; then
-  echo "Replacing existing ${package_name}.zip in this directory"
+  echo "Replacing existing ${package_name}.zip in ${PACKAGE_DIR}"
   rm ./${package_name}.zip
 fi
 
 temp_dir=$(mktemp -d /tmp/${package_name}-build-XXXXX)
 
 cp -r * "${temp_dir}"
-mkdir -p "${temp_dir}"/data/opt/flight-support
+mkdir -p "${temp_dir}"/"${build_dest}"
 
-echo "Creating Forge package of Flight Support from git rev $(git rev-parse --short HEAD)"
+echo "Creating Forge package ${package_name} from git rev $(git rev-parse --short HEAD)"
 
-pushd .. > /dev/null
-git archive HEAD | tar -x -C "${temp_dir}"/data/opt/flight-support
+pushd "${REPO_ROOT}" > /dev/null
+git archive HEAD | tar -x -C "${temp_dir}"/"${build_dest}"
 popd > /dev/null
 
-pushd "${temp_dir}"/data/opt/flight-support > /dev/null
+pushd "${temp_dir}"/"${build_dest}" > /dev/null
 bundle install --without="development test" --path=vendor
 
 rm -rf Rakefile vendor/cache .gitignore README.md
@@ -33,6 +40,8 @@ pushd "${temp_dir}" > /dev/null
 zip -r ${package_name}.zip *
 popd > /dev/null
 
-mv "${temp_dir}"/${package_name}.zip .
+mv "${temp_dir}"/${package_name}.zip "${PACKAGE_DIR}"
 
 rm -rf "${temp_dir}"
+
+echo "Created Forge package ${package_name} (from git rev $(git rev-parse --short HEAD)) at ${PACKAGE_DIR}/${package_name}.zip"
